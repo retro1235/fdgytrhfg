10371075f65ca782da25c62e09f9acf375eb409e
retro1235 <82151737+retro1235@users.noreply.github.com>
2021-09-26 11:03:13 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAEiFqtQDZaWc2SJ8a46qTrzutPyO-FmVo",
+  authDomain: "kwitter-901c3.firebaseapp.com",
+ databaseURL: "https://kwitter-901c3-default-rtdb.firebaseio.com",
+  projectId: "kwitter-901c3",
+ storageBucket: "kwitter-901c3.appspot.com",
+ messagingSenderId: "376147091442",
+  appId: "1:376147091442:web:e4f9f507aaa3dc6684956b",
+ measurementId: "G-FVPDF5K7VZ" 
+         };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
